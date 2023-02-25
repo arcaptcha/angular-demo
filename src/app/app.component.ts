e73e439386f82b01f85b716b13ad01811088a692
hbn.yasaman <hbn.yasaman@gmail.com>
2023-02-25 16:24:07 +0330

    adds test project for angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ArcaptchaAngularComponent } from 'arcaptcha-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'arcaptcha-angular';
+
+  @ViewChild(ArcaptchaAngularComponent)
+  private widget!: ArcaptchaAngularComponent;
+  
+  // public ngOnInit(){
+  //   // this.theBoundCallback = this.theCallback.bind(this);
+  // }
+
+  exec(){
+    this.widget.execute();
+  }
+  callbackFunction(token:any){
+    console.log(token)
+  } 
+  renderCallback(){
+    console.log("rendered:)")
+  }
+  expireCallback(){
+    console.log("expire")
+  }
 }
